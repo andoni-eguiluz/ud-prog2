5347bd5f85630b0a8e1cdb9f51c72af312253a52
Andoni Eguíluz <andoni-eguiluz@users.noreply.github.com>
2022-03-03 17:58:45 +0100

    Clase 3 de marzo

--- Clase2022/src/tema1/clases/bolas/Bola.java
+++ Clase2022/src/tema1/clases/bolas/Bola.java
@@ -1,10 +1,19 @@
 package tema1.clases.bolas;
 
 import java.awt.Color;
+import java.util.Random;
 
 import utils.ventanas.ventanaBitmap.VentanaGrafica;
 
 public class Bola {
+	private static Random random = new Random();
+	private static Color[] colores = { Color.YELLOW, Color.RED, Color.GREEN, Color.BLUE, Color.CYAN, Color.MAGENTA, Color.BLACK }; 
+	
+	// Inicializador static - solo 1 vez al principio
+//	static {
+//		random = new Random();
+//	}
+	
 	private double xCentro;  // x del centro
 	private double yCentro;  // y del centro
 	private int vX;
@@ -42,6 +51,29 @@ public class Bola {
 	 */
 	public Bola() {
 		this( 100, 100, 0, 0, 10, Color.WHITE, Color.BLUE );
+	}
+	
+	/** Crea una bola nueva con datos aleatorios
+	 * Posición: dentro de la ventana
+	 * Radio: entre 10 y 20 pixels
+	 * Velocidad x,y: entre 10 y 100 pixels por segundo (signo aleatorio + o -)
+	 * Color de borde y fondo: aleatorios entre los colores amarillo, rojo, verde, azul, cyan, magenta, negro
+	 * @param v	Ventana en la que crear la bola
+	 */
+	public Bola(VentanaGrafica v) {
+		radio = random.nextInt(11) + 10;
+		xCentro = random.nextInt(v.getAnchura()-radio-radio) + radio;
+		yCentro = random.nextInt(v.getAltura()-radio-radio) + radio;
+		vX = random.nextInt(91) + 10;
+		vY = random.nextInt(91) + 10;
+		if (random.nextBoolean()) {
+			vX = -vX;
+		}
+		if (random.nextBoolean()) {
+			vY = -vY;
+		}
+		colorBorde = colores[ random.nextInt(colores.length) ];
+		colorFondo = colores[ random.nextInt(colores.length) ];
 	}
 
 	public double getX() {
@@ -119,6 +151,9 @@ public class Bola {
 		v.dibujaCirculo( this.getX(), this.getY(), this.getRadio(), 2.0f, Color.WHITE, Color.WHITE );
 	}
 	
+
+	
+	
 	// METODO BLOQUEANTE -- No devuelve el control de forma inmediata (OJO!)
 
 	// Cómo funciona tiempo y frecuencia de refresco?  Son inversos
@@ -126,6 +161,11 @@ public class Bola {
 	// Pausa 10 msgs = 0.010 sgs -> 100 fps -> ¿cuántos avanzo en cada f? 1
 	// Pausa 40 msgs = 0.040 sgs -> 25 fps ->                             4
 	// Pausa               x sgs -> y fps ->                              100/y = 100*x
+	/** Mueve la bola y la rebota en los bordes hasta que se 
+	 * cierre la ventana. NO DEVUELVE EL CONTROL HASTA QUE
+	 * LA VENTANA SE CIERRA
+	 * @param v
+	 */
 	public void mueveYRebota( VentanaGrafica v ) {
 		double tiempoSegs = 0.01;
 		long tiempoMsgs = (long) (tiempoSegs * 1000);
@@ -153,6 +193,27 @@ public class Bola {
 		}
 	}
 	
+	/** Mueve la bola un intervalo de tiempo y la rebota con los 
+	 * bordes si procede
+	 * @param v	Ventana en la que se mueve y rebota
+	 * @param tiempoSegs	Tiempo transcurrido en segundos
+	 */
+	public void mueveYRebota( VentanaGrafica v, double tiempoSegs ) {
+		borrar( v );  // 1. Borramos
+		// 2. Movemos
+		setX( (getvX()*tiempoSegs) + getX() );  // x = x + vX;  // x += vX;
+		setY( (getvY()*tiempoSegs) + getY() );
+		// 3. Detectamos choques
+		if (hayChoqueHorizontal(v)) {
+			vX = -vX;
+		}
+		if (hayChoqueVertical(v)) {
+			vY = -vY;
+		}
+		// 4. Dibujamos
+		dibujar( v );
+	}
+	
 	public boolean hayChoqueHorizontal(VentanaGrafica v) {
 		if (xCentro <= radio) {
 			return true;
@@ -163,8 +224,19 @@ public class Bola {
 		return false;
 	}
 	
-	public boolean hayChoqueVertical( VentanaGrafica v ) {
-		return (yCentro <= radio) || (yCentro >= v.getAltura() - radio);
+	public boolean hayChoqueVertical( VentanaGrafica vent ) {
+		return (yCentro <= radio) || (yCentro >= vent.getAltura() - radio);
+	}
+	
+	/** Comprueba choque de la bola con otra
+	 * @param otraBola	Bola con la que comprobar
+	 * @return	true si se tocan, false si no
+	 */
+	public boolean hayChoqueConBola(Bola otraBola) {
+		double ladox = this.xCentro - otraBola.xCentro;
+		double ladoy = this.yCentro - otraBola.yCentro;
+		double dist = Math.sqrt( ladox*ladox + ladoy*ladoy );
+		return dist <= radio + otraBola.radio;
 	}
 	
 }
--- Clase2022/src/tema1/clases/bolas/MainBolas.java
+++ Clase2022/src/tema1/clases/bolas/MainBolas.java
@@ -1,6 +1,8 @@
 package tema1.clases.bolas;
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 
 import utils.ventanas.ventanaBitmap.VentanaGrafica;
 
@@ -8,6 +10,10 @@ public class MainBolas {
 
 	private static VentanaGrafica v = null;
 	private static Bola bola1;
+
+	// TODO
+	// Bola[] bolas
+	ArrayList<Bola> listaBolas = new ArrayList<Bola>();
 	
 	public static void main(String[] args) {
 		// pruebas();
@@ -18,10 +24,36 @@ public class MainBolas {
 	private static void rebotarBolas() {
 		v = new VentanaGrafica( 1000, 600, "Bolitas" );
 		v.getJFrame().setLocation( 2000, 0 );  // Segunda pantalla
-		bola1 = new Bola( 200, 200, -900, 100, 25, Color.CYAN, Color.GREEN );
-		bola1.mueveYRebota( v );
+		bola1 = new Bola( 200, 500, -900, 100, 25, Color.CYAN, Color.GREEN );
 		Bola bola2 = new Bola(300, 300, 100, 100, 20, Color.BLUE, Color.YELLOW );
-		bola2.mueveYRebota( v );
+		double tiempoSgs = 0.01;
+		long tiempoMsgs = (long) (tiempoSgs * 1000);
+		// BUCLE DE TIEMPO REAL
+		// INPUT - PROCESS - OUTPUT
+		Bola bola3 = null;
+		while (!v.estaCerrada()) {
+			if ( v.getCodUltimaTeclaTecleada()==KeyEvent.VK_PLUS ) {  // Tecla +
+				// Crear bola nueva
+				System.out.println( "Tecla +" );
+				bola3 = new Bola(v);
+				// listaBolas.add( bola3 );
+			}
+			// Mover y rebotar en bordes
+			bola1.mueveYRebota( v, tiempoSgs );
+			bola2.mueveYRebota( v, tiempoSgs );
+			if (bola3!=null) {
+				bola3.mueveYRebota(v, tiempoSgs );
+			}
+			// Choque y rebote entre bolas
+			if (bola1.hayChoqueConBola(bola2)) {
+				// Rebote
+				bola1.setvX( -bola1.getvX() );
+				bola1.setvY( -bola1.getvY() );
+				bola2.setvX( -bola2.getvX() );
+				bola2.setvY( -bola2.getvY() );
+			}
+			v.espera( tiempoMsgs );
+		}
 	}
 	
 	private static void mover() {
